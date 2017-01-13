588ccb442881028554400a9564acfdae8b0a33af
wusuluren <1634636348@qq.com>
2017-01-13 16:55:52 +0800

    fixed dbf support

--- dbf/dbf.go
+++ dbf/dbf.go
@@ -15,25 +15,19 @@ import (
 	"context"
 	"sync"
 	"strconv"
+	"errors"
+	"fmt"
 )
 
 var (
+	flagSHDbf   = flag.String("dbfSH", "SHOW2003.DBF", "SH dbf to watch")
 	flagSZDbf   = flag.String("dbfSZ", "SJSHQ.DBF", "SZ dbf to watch")
-	flagSZxxDbf   = flag.String("dbfSZXX", "SJSXXN.DBF", "SZxx dbf to watch")
+	flagSZxxDbf = flag.String("dbfSZXX", "SJSXXN.DBF", "SZxx dbf to watch")
 	flagDebug   = flag.Bool("debug", false, "debug mode")
 
-	lastUpdateTimeStrSZ string
-
-	updateIdSZ int
-
-	recordData    = map[string]dbfRecord{}
-	lastRecordMap = map[string]Record{}
-	recordsHashSZ = map[string][]byte{}
-
-	suspensionSZ = map[string]bool{}
-
-	sjsxxDuration = 10 * time.Second //time.Minute
+	szxxDuration = 10 * time.Second //time.Minute
 	szDuration = 10 * time.Second //time.Minute
+	shDuration = 10 * time.Second //time.Minute
 )
 
 type FinancialType int
@@ -45,167 +39,259 @@ const (
 	STOCK
 )
 
-type dbfKey struct {
-	name string
-	uid  int
-}
+const (
+	LAST = iota
+	OPEN
+	CLOSE
+	HIGH
+	LOW
+	VOLUME
+)
 
-func (k dbfKey) UID() int {
-	return k.uid
-}
-
-func (k dbfKey) String() string {
-	return k.name
-}
-
-func sendOnce_sz(file Interface) {
+func (m *dbf)sendOnce(stockId string, file Interface) {
 	records := GetRecords(file)
 	timestamp := time.Now()
 	var err error
 
 	//特殊记录
-	sr := records[1]
-	if sr.Data["HQCJSL"] != "0" {
+	var closeField, dateFieldA, dateFieldB string
+	if stockId == "SZ" {
+		closeField = "HQCJSL"
+		dateFieldA = "HQCJBS"
+		dateFieldB = "HQZQJC"
+	} else if stockId == "SH" {
+		closeField = "S11"
+		dateFieldA = "S2"
+		dateFieldB = "S6"
+	} else {
+		log.Warn("Unknown stock")
 		return
 	}
-	timeStr := strings.Trim(sr.Data["HQCJBS"], " ")
+	sr := records[1]
+	if sr.Data[closeField] != "0" {
+		return
+	}
+	timeStr := strings.Trim(sr.Data[dateFieldA], " ")
 	if len(timeStr) < 6 {
 		timeStr = "0" + timeStr
 	}
-	dateTimeStr := strings.Trim(sr.Data["HQZQJC"], " ") + timeStr
+	dateTimeStr := strings.Trim(sr.Data[dateFieldB], " ") + timeStr
 	loc, _ := time.LoadLocation("Asia/Shanghai")
 	timestamp, err = time.ParseInLocation("20060102150405", dateTimeStr, loc)
 	if err != nil {
-		log.Warn("Parse SZ time err. Error message:", err)
+		log.Warn(fmt.Sprintf("Parse %s time err. Error message:", stockId), err)
 		return
 	}
-	if dateTimeStr == lastUpdateTimeStrSZ {
+	if dateTimeStr == m.lastUpdateTimeStr {
 		//TODO 目前不做处理
 		//timestamp.Add(1000)
 	} else {
-		lastUpdateTimeStrSZ = dateTimeStr
+		m.lastUpdateTimeStr = dateTimeStr
 	}
 	//取完信息后删除特殊记录
 	delete(records, 1)
 
+	var targetId string
+	var updateId *int
+	targetFieldsSZ := []string{"HQZRSP", "HQJRKP", "HQZJCJ", "HQZGCJ", "HQZDCJ", "HQCJSL"}
+	targetFieldsSH := []string{"S3", "S4", "S8", "S6", "S7", "S11"}
+	var targetFields []string
+	fieldNames := []int{LAST, OPEN, CLOSE, HIGH, LOW, VOLUME}
+	fieldValues := map[int]float64{}
+	if stockId == "SZ" {
+		targetFields = targetFieldsSZ
+		targetId = "HQZQDM"
+		updateId = &m.updateIdSZ
+	} else if stockId == "SH" {
+		targetFields = targetFieldsSH
+		targetId = "S1"
+		updateId = &m.updateIdSH
+	} else {
+		log.Warn("Unknown stock")
+		return
+	}
 	for _, r := range records {
-		target := r.Data["HQZQDM"] + ".SZ"
+		target := r.Data[targetId] + "." + stockId
+		targetKey, err := key.ParseFromStr("stock/" + target)
+		if err != nil {
+			log.Warn("ParseFromStr err. Error message:", err.Error())
+			continue
+		}
+		targetUid := targetKey.UID()
 		hash := r.HashCode
-		lastHash, exist := recordsHashSZ[target]
+		lastHash, exist := m.recordsHash[targetUid]
 		if !exist || bytes.Compare(hash, lastHash) != 0 {
-			recordsHashSZ[target] = hash
+			m.recordsHash[targetUid] = hash
 		} else {
 			continue
 		}
-
-		last, err := strconv.ParseFloat(r.Data["HQZRSP"], 64)
-		if err != nil {
-			log.Warn("Parse SZ last err. Error message:", err)
-			continue
+		for i, field := range targetFields {
+			fieldValues[fieldNames[i]], err = strconv.ParseFloat(r.Data[field], 64)
+			if err != nil {
+				log.Warn(fmt.Sprintf("Parse %s last err. Error message:", stockId), err.Error())
+				continue
+			}
 		}
-		open, err := strconv.ParseFloat(r.Data["HQJRKP"], 64)
-		if err != nil {
-			log.Warn("Parse SZ open err. Error message:", err)
-			continue
-		}
-		clos, err := strconv.ParseFloat(r.Data["HQZJCJ"], 64)
-		if err != nil {
-			log.Warn("Parse SZ close err. Error message:", err)
-			continue
-		}
-		high, err := strconv.ParseFloat(r.Data["HQZGCJ"], 64)
-		if err != nil {
-			log.Warn("Parse SZ high err. Error message:", err)
-			continue
-		}
-		low, err := strconv.ParseFloat(r.Data["HQZDCJ"], 64)
-		if err != nil {
-			log.Warn("Parse SZ low err. Error message:", err)
-			continue
-		}
-		vol, err := strconv.ParseFloat(r.Data["HQCJSL"], 64)
-		if err != nil {
-			log.Warn("Parse SZ volume err. Error message:", err)
-			continue
-		}
-		tick := recordData[target]
-		tick.suspension = suspensionSZ[target]
+		tick := m.recordData[targetUid]
+		tick.key = targetKey
+		tick.suspension = m.suspension[targetUid]
 		tick.time = timestamp
-		tick.id = int64(updateIdSZ)
+		tick.id = int64(*updateId)
 		tick.productType = STOCK
-		if suspensionSZ[target] || tick.low < 0.00001 || tick.low > 99990.0 {
+		if m.suspension[targetUid] || tick.low < 0.00001 || tick.low > 99990.0 {
 			//如果当前股票是停牌
-			tick.last = last
-			tick.open = last
-			tick.close = last
-			tick.high = last
-			tick.low = last
+			tick.last = fieldValues[LAST]
+			tick.open = fieldValues[LAST]
+			tick.close = fieldValues[LAST]
+			tick.high = fieldValues[LAST]
+			tick.low = fieldValues[LAST]
 			tick.volume = 0
 		} else {
-			tick.last = last
-			tick.open = open
-			tick.close = clos
-			tick.high = high
-			tick.low = low
-			tick.volume = vol
+			tick.last = fieldValues[LAST]
+			tick.open = fieldValues[OPEN]
+			tick.close = fieldValues[CLOSE]
+			tick.high = fieldValues[HIGH]
+			tick.low = fieldValues[LOW]
+			tick.volume = fieldValues[VOLUME]
 		}
-		lastRecordMap[target] = r
-		recordData[target] = tick
-
-		tick.key, err = key.ParseFromStr("stock/" + target)
-		if err != nil {
-			log.Warn("ParserFromStr error:", err.Error())
-		} else {
-			record := &dbfRecord{
-				id: tick.id,
-				time: tick.time,
-				last: tick.last,
-				open: tick.open,
-				high: tick.high,
-				low: tick.low,
-				close: tick.close,
-				volume: tick.volume,
-				key: tick.key,
-				status: tick.status,
-				suspension: tick.suspension,
-				productType: tick.productType,
-			}
-			workingDBF.latestRecords[tick.Key().UID()] = record
+		m.lastRecordMap[targetUid] = r
+		m.recordData[targetUid] = tick
+		m.latestRecords[targetUid] = &dbfRecord{
+			id: tick.id,
+			time: tick.time,
+			last: tick.last,
+			open: tick.open,
+			high: tick.high,
+			low: tick.low,
+			close: tick.close,
+			volume: tick.volume,
+			key: tick.key,
+			status: tick.status,
+			suspension: tick.suspension,
+			productType: tick.productType,
 		}
 	}
-	updateIdSZ++
+	(*updateId)++
 }
 
-func SJSXX() {
-	lastHashSJSXX := []byte{}
+func (m *dbf)SZ(ctx context.Context) error {
+	hasher := md5.New()
+	lastHash := []byte{}
 
 	for {
-		contentSJSXX, err := ioutil.ReadFile(*flagSZxxDbf)
+		content, err := ioutil.ReadFile(*flagSZDbf)
 		if err != nil {
 			log.Warn(err)
 		} else {
-			hasher := md5.New()
-			hash := hasher.Sum(contentSJSXX)
-			if bytes.Compare(hash, lastHashSJSXX) != 0 {
+			hash := hasher.Sum(content)
+			if bytes.Compare(hash, lastHash) != 0 {
+				log.Debug("SZ changed")
+				lastHash = hash
+				reader := bytes.NewReader(content)
+				m.sendOnce("SZ", reader)
+
+				for _, r := range m.latestRecords {
+					m.lock.RLock()
+					if m.onUpdateHandler != nil {
+						m.onUpdateHandler(r)
+					}
+					m.lock.RUnlock()
+				}
+			} else {
+				log.Debug("SZ unchanged")
+			}
+		}
+
+		timer := time.NewTimer(szDuration)
+		select {
+		case <-timer.C:
+			timer.Stop()
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (m *dbf)SH(ctx context.Context) error {
+	hasher := md5.New()
+	lastHash := []byte{}
+
+	for {
+		content, err := ioutil.ReadFile(*flagSHDbf)
+		if err != nil {
+			log.Warn(err)
+		} else {
+			hash := hasher.Sum(content)
+			if bytes.Compare(hash, lastHash) != 0 {
+				log.Debug("SH changed")
+				lastHash = hash
+				reader := bytes.NewReader(content)
+				m.sendOnce("SH", reader)
+
+				for _, r := range m.latestRecords {
+					m.lock.RLock()
+					if m.onUpdateHandler != nil {
+						m.onUpdateHandler(r)
+					}
+					m.lock.RUnlock()
+				}
+			} else {
+				log.Debug("SH unchanged")
+			}
+		}
+
+		timer := time.NewTimer(shDuration)
+		select {
+		case <-timer.C:
+			timer.Stop()
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (m *dbf)SJSXX(ctx context.Context) error {
+	hasher := md5.New()
+	lastHash := []byte{}
+
+	for {
+		content, err := ioutil.ReadFile(*flagSZxxDbf)
+		if err != nil {
+			log.Warn(err)
+		} else {
+			hash := hasher.Sum(content)
+			if bytes.Compare(hash, lastHash) != 0 {
 				log.Debug("SJSXX changed")
-				reader := bytes.NewReader(contentSJSXX)
+				reader := bytes.NewReader(content)
 
 				records := GetRecords(reader)
 				delete(records, 1)
 				for _, r := range records {
 					target := r.Data["XXZQDM"] + ".SZ"
+					targetKey, err := key.ParseFromStr("stock/" + target)
+					if err != nil {
+						log.Warn("ParseFromStr err. Error message:", err.Error())
+					}
+					targetUid := targetKey.UID()
 					if r.Data["XXTPBZ"] == "T" {
-						suspensionSZ[target] = true
+						m.suspension[targetUid] = true
 					} else {
-						suspensionSZ[target] = false
+						m.suspension[targetUid] = false
 					}
 				}
-				lastHashSJSXX = hash
+				lastHash = hash
 			} else {
 				log.Debug("SJSXX unchanged")
 			}
 		}
-		time.Sleep(sjsxxDuration)
+
+		timer := time.NewTimer(szxxDuration)
+		select {
+		case <-timer.C:
+			timer.Stop()
+		case <-ctx.Done():
+			return nil
+		}
 	}
 }
 
@@ -265,10 +351,20 @@ func (record *dbfRecord) MarshalJSON() ([]byte, error) {
 }
 
 type dbf struct {
-	lock          	sync.RWMutex
-	latestRecords 	map[int]*dbfRecord
-	onUpdateHandler	func(market.Record)
+	lock          		sync.RWMutex
+	recordData   		map[int]dbfRecord
+	lastRecordMap		map[int]Record
+	recordsHash 		map[int][]byte
+	latestRecords		map[int]*dbfRecord
+	onUpdateHandler		func(market.Record)
+	lastUpdateTimeStr 	string
+	updateIdSZ 			int
+	updateIdSH 			int
+	suspension			map[int]bool
 }
+
+// ErrBusy 该服务忙, DBF一次只能承担一个连接
+var ErrBusy = errors.New("dbf is busy")
 
 func (m *dbf) Run(ctx context.Context) error {
 	flag.Parse()
@@ -278,39 +374,29 @@ func (m *dbf) Run(ctx context.Context) error {
 		log.SetLevel(log.InfoLevel)
 	}
 
-	hasher := md5.New()
-	lastHashSZ := []byte{}
-	workingDBF = m
+	select {
+	case <-dbfAvailable:
+	default:
+		return ErrBusy
+	}
+	defer func() {
+		dbfAvailable <- true
+	}()
+	log.Debug(time.Now().String(), ": Try to connect with dbf server")
 
-	go SJSXX()
+	go m.SJSXX(ctx)
+	go m.SZ(ctx)
+	go m.SH(ctx)
 
 	for {
-		time.Sleep(szDuration)
-
-		contentSZ, err := ioutil.ReadFile(*flagSZDbf)
-		if err != nil {
-			log.Warn(err)
-		} else {
-			hash := hasher.Sum(contentSZ)
-			if bytes.Compare(hash, lastHashSZ) != 0 {
-				log.Debug("SZ changed")
-				lastHashSZ = hash
-				reader := bytes.NewReader(contentSZ)
-				sendOnce_sz(reader)
-
-				for _, r := range m.latestRecords {
-					m.lock.RLock()
-					if m.onUpdateHandler != nil {
-						m.onUpdateHandler(r)
-					}
-					m.lock.RUnlock()
-				}
-			} else {
-				log.Debug("SZ unchanged")
-			}
+		timer := time.NewTimer(szDuration)
+		select {
+		case <-timer.C:
+			timer.Stop()
+		case <-ctx.Done():
+			return nil
 		}
 	}
-	return nil
 }
 
 func (m *dbf) OnUpdate(onUpdate func(market.Record)) {
@@ -333,7 +419,6 @@ func (m *dbf) LatestAll() []market.Record {
 	m.lock.RLock()
 	defer m.lock.RUnlock()
 	records := make([]market.Record, len(m.latestRecords))
-	log.Debug(len(m.latestRecords))
 	idx := 0
 	for _, r := range m.latestRecords {
 		records[idx] = r
@@ -342,14 +427,21 @@ func (m *dbf) LatestAll() []market.Record {
 	return records
 }
 
-var workingDBF *dbf
+var dbfAvailable = make(chan bool, 1)
 
 func initDBF(url *url.URL) (market.Market, error) {
 	return &dbf{
-		latestRecords: map[int]*dbfRecord{},
+		recordData:		map[int]dbfRecord{},
+		lastRecordMap:	map[int]Record{},
+		recordsHash:	map[int][]byte{},
+		latestRecords: 	map[int]*dbfRecord{},
+		suspension: 	map[int]bool{},
+		updateIdSZ:		0,
+		updateIdSH:		0,
 	}, nil
 }
 
 func init() {
 	market.Markets[DBFScheme] = initDBF
+	dbfAvailable <- true
 }
